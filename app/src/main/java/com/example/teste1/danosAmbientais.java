0ea117eee8cb7362ee00d70f7fb933c39be234d2
Rafael-Wassoaski <meireles200@hotmail.com>
2019-08-06 20:55:50 -0300

    começo da validação

--- app/src/main/java/com/example/teste1/danosAmbientais.java
+++ app/src/main/java/com/example/teste1/danosAmbientais.java
@@ -6,12 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class danosAmbientais extends Fragment {
+
+
+    EditText desalojadosQuant;
+    EditText desabrigadosQuant;
+    EditText contamincaoAr;
+
 
 
     public danosAmbientais() {
@@ -23,7 +31,21 @@ public class danosAmbientais extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        desalojadosQuant = (EditText) getView().findViewById(R.id.desabrigadosQuant);
+        desabrigadosQuant = (EditText) getView().findViewById(R.id.desabrigadosQuant);
+        contamincaoAr = (EditText) getView().findViewById(R.id.contamincaoAr);
+
+
         return inflater.inflate(R.layout.fragment_danos_ambientais, container, false);
+    }
+
+
+    public boolean validate(){
+
+        if(desalojadosQuant)
+
+
+        return true;
     }
 
 }
